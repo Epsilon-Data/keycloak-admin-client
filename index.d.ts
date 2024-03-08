6f82bde815f798a1417a76c8b29fe84242d42dc2
Aare Puussaar <aare.puussaar@gmail.com>
2024-03-08 16:43:47 +0000

    feat: added additional lib imports

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,11 @@
-import Keycloak from '@keycloak/keycloak-admin-client';
+import KeycloakAdminClient from "@keycloak/keycloak-admin-client";
+import { Credentials } from "@keycloak/keycloak-admin-client/lib/utils/auth";
+import UserRepresentation from "@keycloak/keycloak-admin-client/lib/defs/userRepresentation";
+import EventRepresentation from "@keycloak/keycloak-admin-client/lib/defs/eventRepresentation";
 
-export { Keycloak };
+export {
+  KeycloakAdminClient,
+  Credentials,
+  UserRepresentation,
+  EventRepresentation,
+};
