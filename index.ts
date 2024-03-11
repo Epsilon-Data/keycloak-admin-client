67ecf9f23de8c2aa35d300ebd739800dfd92fd03
Aare Puussaar <aare.puussaar@gmail.com>
2024-03-11 14:28:30 +0000

    Added additional deps + version UP

--- index.ts
+++ index.ts
@@ -2,10 +2,15 @@ import KeycloakAdminClient from "@keycloak/keycloak-admin-client";
 import { Credentials } from "@keycloak/keycloak-admin-client/lib/utils/auth";
 import UserRepresentation from "@keycloak/keycloak-admin-client/lib/defs/userRepresentation";
 import EventRepresentation from "@keycloak/keycloak-admin-client/lib/defs/eventRepresentation";
-
+import RoleRepresentation from "@keycloak/keycloak-admin-client/lib/defs/roleRepresentation";
+import ClientRepresentation from "@keycloak/keycloak-admin-client/lib/defs/clientRepresentation";
+import ClientScopeRepresentation from "@keycloak/keycloak-admin-client/lib/defs/clientScopeRepresentation";
 export {
   KeycloakAdminClient,
   Credentials,
   UserRepresentation,
   EventRepresentation,
+  RoleRepresentation,
+  ClientRepresentation,
+  ClientScopeRepresentation,
 };
